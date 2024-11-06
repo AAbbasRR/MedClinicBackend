59ebb4f773706cd4f64acc243d3b422f2cf2beb1
Abbas Rahimzadeh <arahimzadeh79@gmail.com>
2024-11-06 20:44:36 +0330

    bugfix: fixed time 7 days to 6 days

--- src/app_doctor/api/public/serializers/datetimes.py
+++ src/app_doctor/api/public/serializers/datetimes.py
@@ -8,16 +8,6 @@ from utils.serializers import CustomModelSerializer
 from datetime import datetime, timedelta
 import jdatetime
 
-days_in_persian = {
-    0: "دوشنبه",
-    1: "سه‌شنبه",
-    2: "چهارشنبه",
-    3: "پنج‌شنبه",
-    4: "جمعه",
-    5: "شنبه",
-    6: "یک‌شنبه",
-}
-
 
 class UsersDoctorDateTimeModelSerializer(CustomModelSerializer):
     class Meta:
@@ -30,7 +20,7 @@ class UsersDoctorDateTimeModelSerializer(CustomModelSerializer):
 
         today_gregorian = make_aware(datetime.now())
         today_jalali = jdatetime.date.fromgregorian(date=today_gregorian.date())
-        seven_days_later_jalali = today_jalali + timedelta(days=7)
+        seven_days_later_jalali = today_jalali + timedelta(days=6)
 
         if ReservationModel.objects.filter(
             doctor=instance.doctor,
